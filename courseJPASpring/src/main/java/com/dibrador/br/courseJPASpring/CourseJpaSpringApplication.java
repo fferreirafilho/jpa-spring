5be3cf905a52d69ed37ad0f4ee5eed9a5211636d
Fernando Ferreira <fernandoferreirrafilho@gmail.com>
2023-02-13 07:27:05 -0300

    Firs commit

---
+++ courseJPASpring/src/main/java/com/dibrador/br/courseJPASpring/CourseJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dibrador.br.courseJPASpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseJpaSpringApplication.class, args);
+	}
+
+}
---
+++ courseJPASpring/src/test/java/com/dibrador/br/courseJPASpring/CourseJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibrador.br.courseJPASpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
